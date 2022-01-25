497edd0522192da743c076d11d804fcc8fb3d843
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-01-25 17:34:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let nav = document.querySelector('#navbar')
 
 button.onclick = function (){
   if(nav.style.display == 'none'){
-     nav.style.display = 'flex'
+    button.innerText = 'X';button.style.paddingLeft = '35%';nav.style.display = 'flex'
+    button.style.backgroundColor = '#550055'
     return
   }
-  nav.style.display = 'none'
+  button.style.paddingLeft = '0%';button.innerText = 'Menu';
+  button.style.backgroundColor = 'rgb(40, 58, 73)';nav.style.display = 'none'
 }
