695c4131c7d1c8403704211b7a42724fd30142de
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-02-24 21:22:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let button = document.querySelector('#drop-down-btn')
+let nav = document.querySelector('#navbar')
 
+console.log(getComputedStyle(nav).display)
+button.onclick = function (){
+  if(getComputedStyle(nav).display === 'flex'){
+    nav.style.display = 'none'
+    
+    
+  }
+  else{
+    console.log('none')
+    nav.style.display = 'flex'
+  }
+}
+
+window.addEventListener('resize', function (){
+  let val = this.getComputedStyle(document.body).width;
+  val = val.match(/\d+/)[0]
+  console.log(val)
+  if(val > 1000){
+    nav.style.display = 'flex'
+  }else{
+    nav.style.display = 'none'
+  }
+   
+})
