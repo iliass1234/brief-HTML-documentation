9fe67a177c70088e2e3ac80477b859b541da879e
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-02-24 20:45:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let button = document.querySelector('#drop-down-btn')
-let nav = document.querySelector('#navbar')
 
-button.onclick = function (){
-  if(nav.style.display == 'none'){
-    button.innerText = 'X';button.style.paddingLeft = '35%';nav.style.display = 'flex'
-    button.style.backgroundColor = '#550055'
-    return
-  }
-  button.style.paddingLeft = '0%';button.innerText = 'Menu';
-  button.style.backgroundColor = 'rgb(40, 58, 73)';nav.style.display = 'none'
-}
