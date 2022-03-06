2402a83d2ed93504aa44cf15cc6b7cad674f561f
ilias jabri <expert.ilyasse@outlook.com>
2022-03-06 21:09:47 +0100

    final touch

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-let button = document.querySelector('#drop-down-btn')
-let nav = document.querySelector('#navbar')
+let button = document.querySelector('#drop-down-btn');
+let nav = document.querySelector('#navbar');
 
 console.log(getComputedStyle(nav).display)
 button.onclick = function (){
   if(getComputedStyle(nav).display === 'flex'){
-    nav.style.display = 'none'
-    button.innerText = '...'
-
+    nav.style.display = 'none';
+    button.innerText = '...';
+    button.style.backgroundColor = 'rgb(40, 58, 73)';
   }else{
+    button.style.backgroundColor = 'rgb(20, 30, 40)';
     console.log('none')
-    nav.style.display = 'flex'
-    button.innerText = 'x'
+    nav.style.display = 'flex';
+    button.innerText = 'x';
 
   }
 }
 
 window.addEventListener('resize', function (){
   let val = this.getComputedStyle(document.body).width;
-  val = val.match(/\d+/)[0]
-  console.log(val)
-  if(val > 1000){
-    nav.style.display = 'flex'
+  val = val.match(/\d+/)[0];
+  console.log(typeof val, val)
+  if(Number(val) > 983){
+    nav.style.display = 'flex';
   }else{
-    nav.style.display = 'none'
+    nav.style.display = 'none';
   }
    
 })
